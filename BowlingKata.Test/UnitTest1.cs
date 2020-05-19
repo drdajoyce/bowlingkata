ec5a7472faf30f0dbd7e14865d755be28d6c7e26
Duncan Joyce <Duncan_Joyce@waters.com>
2020-05-19 15:41:20 +0100

    First commit

---
+++ BowlingKata.Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+
+namespace BowlingKata.Test
+{
+    public class Tests
+    {
+
+        [Test]
+        public void WhenAGameStartsScoreIsZero()
+        {
+            //Arrange
+            var newGameInstance = new BowlingKata.Game();
+
+            //Act
+            var score = newGameInstance.Score();
+
+            //Assert
+            Assert.AreEqual(score , 0);
+        }
+
+        [TestCase(1,1)]
+        [TestCase(5, 5)]
+        [TestCase(0, 0)]
+        public void WhenAPlayerHitsSomePins_ScoreIsEqualToThat(int pins, int expectedScore)
+        {
+            //Arrange
+            var newGameInstance = new BowlingKata.Game();
+
+            //Act
+            newGameInstance.Roll(pins);
+            var actualScore = newGameInstance.Score();
+
+            //Assert
+            Assert.AreEqual(expectedScore, actualScore);
+        }
+
+      
+
+        [TestCase(3,3,6)]
+        [TestCase(6, 4, 10)]
+        public void WhenAPlayerHits3and3_ScoreIsEqualto6(int firstRollPins,int secondRollPins, int expectedScore)
+        {
+            //Arrange
+            var newGameInstance = new BowlingKata.Game();
+
+            //Act
+            newGameInstance.Roll(firstRollPins);
+            newGameInstance.Roll(secondRollPins);
+            var score = newGameInstance.Score();
+
+            //Assert
+            Assert.AreEqual(expectedScore, score);
+        }
+
+
+    }
+}
---
+++ BowlingKata/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        int _score = 0;
+
+        public void Roll(int pins)
+        {
+            _score += pins;
+        }
+
+        public int Score()
+        {
+            return _score;
+        }
+    }
+}
